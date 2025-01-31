bf61857d679a7042c3f7310c9f7fea712a7d6809
steapn8 <145757703+steapn8@users.noreply.github.com>
2025-01-31 20:06:25 +0300

    засунул код в иф мэин

--- tg_bot_management.py
+++ tg_bot_management.py
@@ -4,20 +4,25 @@ import random
 import argparse
 import os
 from dotenv import load_dotenv
-load_dotenv()
-tg_token = os.getenv('TG_TOKEN')
-bot = telegram.Bot(token = tg_token)
-updates = bot.get_updates()
-parser = argparse.ArgumentParser(
-    description='Программа отправляет все фото из директории с задержкой, которую задают при запуске в секундах.'
-)
-parser.add_argument('delay', help='Задержка между отпрвкой фото в секундах', type=int, default = 14400)
-args = parser.parse_args()
-while True:
-    for dirpath, dirnames, filenames in os.walk("images"):
-        random.shuffle(filenames)
-        for filename in filenames:
-            path = f"images/{filename}"
-            bot.send_document(chat_id=os.getenv('CHAT_ID'), document=open(path, "rb"))
-            time.sleep(2)
-    time.sleep(args.delay)
+
+def main():
+    load_dotenv()
+    tg_token = os.getenv('TG_TOKEN')
+    bot = telegram.Bot(token = tg_token)
+    parser = argparse.ArgumentParser(
+        description='Программа отправляет все фото из директории с задержкой, которую задают при запуске в секундах.'
+    )
+    parser.add_argument('delay', help='Задержка между отпрвкой фото в секундах', type=int, default = 14400)
+    args = parser.parse_args()
+    while True:
+        for dirpath, dirnames, filenames in os.walk("images"):
+            random.shuffle(filenames)
+            for filename in filenames:
+                path = f"images/{filename}"
+                bot.send_document(chat_id=os.getenv('CHAT_ID'), document=open(path, "rb"))
+                time.sleep(2)
+        time.sleep(args.delay)
+
+
+if __name__ == '__main__':
+    main()
