7cef82bda4bf821fd3f8561ef17884dba48490eb
steapn8 <145757703+steapn8@users.noreply.github.com>
2025-02-14 19:12:39 +0300

    сменил регистр букв

--- fetch_NASA_EPIC_images.py
+++ fetch_NASA_EPIC_images.py
@@ -14,15 +14,15 @@ def main():
 
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    image_EPIC = response.json()
-    for number, fug in enumerate(image_EPIC):
+    image_epic = response.json()
+    for number, fug in enumerate(image_epic):
         
-        image_EPIC_date_and_seconds = fug["date"].split()
-        image_EPIC_date = image_EPIC_date_and_seconds[0].split('-')
-        url_EPIC = f"https://api.nasa.gov/EPIC/archive/natural/{image_EPIC_date[0]}/{image_EPIC_date[1]}/{image_EPIC_date[2]}/png/{fug['image']}.png"
+        image_epic_date_and_seconds = fug["date"].split()
+        image_epic_date = image_epic_date_and_seconds[0].split('-')
+        url_epic = f"https://api.nasa.gov/EPIC/archive/natural/{image_epic_date[0]}/{image_epic_date[1]}/{image_epic_date[2]}/png/{fug['image']}.png"
         filename = f'EPIC_{number}.png'
         filepath = f"images/{filename}"
-        download_image(filepath, url_EPIC, api_key)
+        download_image(filepath, url_epic, api_key)
 
 
 if __name__ == '__main__':
