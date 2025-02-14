59c1183b8b627d1b3858f1c00b1912cdc7adc3a8
steapn8 <145757703+steapn8@users.noreply.github.com>
2025-02-14 19:28:04 +0300

    добавил отступ

--- downloading_image.py
+++ downloading_image.py
@@ -6,7 +6,7 @@ import os
 def download_image(filepath, url, api_key = ''):
     Path("images").mkdir(parents=True, exist_ok=True)
     payload = {
-    "api_key": api_key,
+        "api_key": api_key,
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
